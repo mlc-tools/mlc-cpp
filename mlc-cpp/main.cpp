214bf1aa8bf37171e0972e9f8e229623dff8349b
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-07-27 08:16:09 +0700

    Initial Commit

---
+++ mlc-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mlc-cpp
+//
+//  Created by Vladimir Tolmachev on 27.07.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
