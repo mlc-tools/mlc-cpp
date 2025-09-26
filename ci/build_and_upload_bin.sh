1bb71ed60343e051e61aac2db913bdea28f29279
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-09-26 13:17:15 +0700

    added build_an_upload.sh

---
+++ ci/build_and_upload_bin.sh
@@ -0,0 +1,58 @@
+pushd . > /dev/null
+
+function increment_version(){
+	version=$(grep 'APP_VERSION' ../mlc-cpp/version.hpp | sed -E 's/.*"([^"]+)".*/\1/')
+	# echo "Текущая версия: $version"
+
+	# 2. Разбираем версию на части
+	IFS='.' read -r major minor patch <<< "$version"
+
+	# 3. Увеличиваем "patch" (последний номер)
+	patch=$((patch + 1))
+
+	# 4. Собираем новую версию
+	new_version="$major.$minor.$patch"
+
+	# echo "Новая версия: $new_version"
+
+	# 5. Записываем в файл (если нужно переписать version.hpp)
+	# sed -i работает по-разному на Linux и macOS, поэтому:
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+	  sed -i '' -E "s/(APP_VERSION = \").*(\";)/\1$new_version\2/" ../mlc-cpp/version.hpp
+	else
+	  sed -i -E "s/(APP_VERSION = \").*(\";)/\1$new_version\2/" ../mlc-cpp/version.hpp
+	fi
+	
+	echo "$new_version"
+}
+
+function build(){
+	mkdir -p ../build_cli
+	cd ../build_cli
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+	make -j8
+	cmake --install .
+}
+
+# $1 - version
+function upload(){
+	git clone git@github.com:mlc-tools/mlc-binary-app-versions.git bin --depth 1
+	cp mlc bin/
+
+	cd bin
+	git checkout -b versions
+	MESSAGE="osx/$1"
+	git add mlc
+	git commit -m "added binary $MESSAGE"
+	git tag $MESSAGE
+	git push --all -f
+	git push origin versions
+	git push origin --tags
+}
+
+VERSION=$(increment_version)
+echo $VERSION
+build
+upload $VERSION
+
+popd > /dev/null
