94641df02bb15d2b22865b6731ae3eb79ec1aa2c
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-09-27 09:29:55 +0700

    added actual simple test

---
+++ tests/simple_test/test_php.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+if((@include 'generated_php/mg.php') === false) {
+    require_once 'generated_php/DataStorage.php';
+    require_once 'generated_php/AllTests.php';
+    require_once 'generated_php/RunAllTests.php';
+    require_once 'generated_php/Logger.php';
+    require_once 'generated_php/config.php';
+}
+
+$file = 'data.xml';
+if(!Config::$SUPPORT_XML_PROTOCOL){
+	$file = 'data.json';
+}
+
+$file = realpath(dirname(__FILE__))."/assets/$file";
+
+DataStorage::$PATH_TO_DATA = $file;
+DataStorage::shared()->loadAllDataUnits();
+
+class LoggerImpl extends Logger {
+	function message(string $message) {
+		echo ($message."\n");
+	}
+};
+
+$logger = new LoggerImpl();
+
+$result = true;
+$result = AllTests::run($logger) and $result;
+
+$tests = new RunAllTests();
+$tests->initialize($logger);
+$result = $result && $tests->execute();
+
+echo ("\nTests Ok\n");
+exit($result == true?0:1);
+
+?>
---
+++ tests/simple_test/test_py.py
@@ -0,0 +1,43 @@
+import os.path as fs
+import sys
+
+try:
+    from generated_py.AllTests import AllTests
+    from generated_py.DataStorage import DataStorage
+    from generated_py.RunAllTests import RunAllTests
+    from generated_py.Logger import Logger
+except ImportError:
+    from generated_py.mg import *
+
+
+class LoggerImpl(Logger):
+
+    def message(self, message):
+        print(message)
+
+
+def initialize_data_storage(protocol):
+    filepath = fs.abspath(fs.dirname(__file__)) + '/assets/data.' + protocol
+    with open(filepath) as f:
+        content = f.read()
+    if protocol == 'json':
+        DataStorage.shared().initialize_json(content)
+    elif protocol == 'xml':
+        DataStorage.shared().initialize_xml(content)
+
+
+def main(argv):
+    initialize_data_storage(argv[1] if len(argv) > 1 else 'xml')
+
+    logger = LoggerImpl()
+    result = AllTests.run(logger)
+
+    tests = RunAllTests()
+    tests.initialize(logger)
+    result = result and tests.execute()
+
+    sys.exit(0 if result else -1)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
